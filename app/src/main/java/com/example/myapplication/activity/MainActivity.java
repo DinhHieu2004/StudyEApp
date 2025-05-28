01a741644d06ca13a700b51ee492f797f6c64318
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-05-28 16:44:19 +0700

    complete nav bar ui in the bottom

--- app/src/main/java/com/example/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.myapplication.activity;
 
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.R;
 
@@ -18,8 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ConstraintLayout btn1 = findViewById(R.id.mainLayout);
-        btn1.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CoursesListActivity.class)));
+        btn1.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, CoursesListActivity.class))
+        );
 
+        // Hỗ trợ layout toàn màn hình và tránh che bottom nav
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        View bottomNav = findViewById(R.id.bottomNavigationView);
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNav, (v, insets) -> {
+            v.setPadding(0, 0, 0, insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom);
+            return insets;
+        });
     }
 
 }
