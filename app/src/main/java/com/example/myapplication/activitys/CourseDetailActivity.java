3b0c5611a10abedf91731e5d94bba4347acad2d9
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-05-29 21:33:35 +0700

    create class logic handle for scene detail

--- app/src/main/java/com/example/myapplication/activitys/CourseDetailActivity.java
+++ app/src/main/java/com/example/myapplication/activitys/CourseDetailActivity.java
@@ -1,4 +1,46 @@
 package com.example.myapplication.activitys;
 
-public class CourseDetailActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class CourseDetailActivity extends AppCompatActivity {
+
+    private boolean isExpanded = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scene_detail);
+
+        TextView textDialogue = findViewById(R.id.textDialogue);
+        TextView btnMore = findViewById(R.id.btnMore);
+
+        btnMore.setOnClickListener(v -> {
+            isExpanded = !isExpanded;
+            if (isExpanded) {
+                textDialogue.setMaxLines(Integer.MAX_VALUE);
+                btnMore.setText("Less");
+            } else {
+                textDialogue.setMaxLines(4);
+                btnMore.setText("More...");
+            }
+        });
+
+        ImageView btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(v -> {
+            Intent intent = new Intent(CourseDetailActivity.this, SceneLearnActivity.class);
+            intent.putExtra("fragment_to_show", "scene_learn");
+            startActivity(intent);
+            finish();
+        });
+    }
 }
+
