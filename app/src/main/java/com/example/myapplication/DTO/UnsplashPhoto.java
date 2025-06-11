25671ea3e39796e4e2a95f301352bf6469b4ddd1
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-06-11 23:16:51 +0700

    create upslash image search model

--- app/src/main/java/com/example/myapplication/DTO/UnsplashPhoto.java
+++ app/src/main/java/com/example/myapplication/DTO/UnsplashPhoto.java
@@ -1,4 +1,17 @@
 package com.example.myapplication.DTO;
 
 public class UnsplashPhoto {
+    private Urls urls;
+
+    public Urls getUrls() {
+        return urls;
+    }
+
+    public static class Urls {
+        private String small;
+        public String getSmall() {
+            return small;
+        }
+    }
 }
+
--- app/src/main/java/com/example/myapplication/DTO/response/UnsplashResponse.java
+++ app/src/main/java/com/example/myapplication/DTO/response/UnsplashResponse.java
@@ -1,4 +1,13 @@
 package com.example.myapplication.DTO.response;
 
+import com.example.myapplication.DTO.UnsplashPhoto;
+
+import java.util.List;
+
 public class UnsplashResponse {
+    private List<UnsplashPhoto> results;
+
+    public List<UnsplashPhoto> getResults() {
+        return results;
+    }
 }
